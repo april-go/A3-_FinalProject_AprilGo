c52d6bff526e494edbaf0d4c543ea7f3cee9b30b
april-go <71276019+april-go@users.noreply.github.com>
2021-04-23 17:31:11 -0400

    star rating

--- js/script.js
+++ js/script.js
@@ -110,6 +110,7 @@ function contentNav(e) {
 		
 		
   } 
+  
 var exampleModal = document.getElementById('exampleModal')
 exampleModal.addEventListener('show.bs.modal', function (event) {
 
@@ -124,19 +125,18 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
   modalBodyInput.value = recipient
 })
 
-filterSelection("all") // Execute the function and show all columns
+filterSelection("all") 
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
 function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -148,7 +148,7 @@ function w3AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
+
 function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -161,7 +161,7 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
+
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
